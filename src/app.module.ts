07cc961370df81b8f2863397e7da1a2ceee49542
vohuy <vhhuy.c3tqcap.a7@gmail.com>
2020-02-04 22:41:14 +0700

    add graphql typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { GraphQLModule } from '@nestjs/graphql'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { join } from 'path'
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      playground: true
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      host: '35.222.185.187',
+      port: 27017,
+      database: 'tqcSocial',
+      // url: '',
+      useNewUrlParser: true,
+      entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+      synchronize: true,
+      logging: true
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
