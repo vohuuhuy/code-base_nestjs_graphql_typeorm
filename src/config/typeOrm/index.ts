30b82313cd72c9eccd8905ce16ff53a81ee81968
vohuuhuy <vhhuy.c3tqcap.a7@gmail.com>
2020-09-30 22:59:59 +0700

    add variables enviroment

--- src/config/typeOrm/index.ts
+++ src/config/typeOrm/index.ts
@@ -10,12 +10,13 @@ const TypeOrm: TypeOrmModuleOptions = {
   entities: ['dist/**/*.entity{.ts,.js}'],
   keepConnectionAlive: true,
   synchronize: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 
-  // username: '',
-  // password: '',
+  username: R.Variables.ORM_DB_USERNAME,
+  password: R.Variables.ORM_DB_PASSWORD,
   // name: 'mainDb', // use name database must use createConnect('<name>') of typeorm
-  // logging: true,
+  logging: R.Variables.ORM_DB_LOGIN,
+  authSource: R.Variables.ORM_DB_AUTHSOURCE,
   // url: '',
 }
 
--- src/resource/variables/index.ts
+++ src/resource/variables/index.ts
@@ -10,6 +10,10 @@ export const PORT = process.env.PORT || 3001
 export const ORM_DB_HOST = process.env.ORM_HOST || 'localhost'
 export const ORM_DB_PORT: number = parseInt(process.env.ORM_DB_PORT) || 27017
 export const ORM_DB_NAME = process.env.ORM_DB_NAME
+export const ORM_DB_LOGIN: boolean = process.env.ORM_DB_LOGIN && process.env.ORM_DB_LOGIN.toLowerCase() === 'y' ? true : false
+export const ORM_DB_USERNAME = process.env.ORM_DB_USERNAME
+export const ORM_DB_PASSWORD = process.env.ORM_DB_PASSWORD
+export const ORM_DB_AUTHSOURCE: string = process.env.ORM_DB_AUTHSOURCE
 
 // graphql
 export const GRAPHQL_PATH = process.env.GRAPHQL_PATH || '/graphql'
